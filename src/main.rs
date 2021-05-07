92f491a24b0739895f931a91cb7016526ce24223
neuring <neuring@pm.me>
2021-05-07 23:34:26 +0200

    binary search to find best solution

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,12 @@ use std::{
 };
 
 use anyhow::Context;
-use itertools::{iproduct, Either};
-use sat_encoder::{Model, constraints::{And, AtMostK, AtleastK, ExactlyK, Expr, If, Not, Or}, prelude::*};
+use itertools::{iproduct, Itertools};
+use sat_encoder::{
+    constraints::{And, AtMostK, AtleastK, ExactlyK, Expr, If, Not, Or, SameCardinality},
+    prelude::*,
+    Model,
+};
 use structopt::StructOpt;
 use strum::IntoEnumIterator;
 
@@ -21,6 +25,8 @@ enum Color {
 }
 
 impl Color {
+    const END_COLOR: &'static str = "\u{1b}[0m";
+
     fn ascii_color_code(&self) -> &'static str {
         match self {
             Color::Red => "\u{1b}[31m",
@@ -66,6 +72,7 @@ enum Card {
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, derive_more::From, PartialOrd, Ord)]
 enum Var {
+    Chosen(Card, u32),
     Num(NumberBlock),
     Color(ColorBlock),
 }
@@ -76,12 +83,14 @@ fn encode_number_block_rules(encoder: &mut DefaultEncoder<Var>, color: Color, in
 
     // The same space cannot be occupied by its number card *and* joker
     for index in 1..=13 {
-        let lits = [true, false].iter().map(|&joker| Pos(Var::Num(NumberBlock {
-            color,
-            instance,
-            is_joker: joker,
-            index,
-        })));
+        let lits = [true, false].iter().map(|&joker| {
+            Pos(Var::Num(NumberBlock {
+                color,
+                instance,
+                is_joker: joker,
+                index,
+            }))
+        });
 
         encoder.add_constraint(Not(And(lits.clone())));
 
@@ -178,62 +187,120 @@ fn encode_general_rules(solver: &mut DefaultEncoder<Var>) {
     }
 }
 
-fn encode_config(encoder: &mut DefaultEncoder<Var>, config: Config) {
-    for (card, count) in config.cards.into_iter() {
-        let lits = match card {
-            Card::Normal { color, number } => {
-                let left = (0..=1)
-                    .map(move |inst| {
-                        Var::Num(NumberBlock {
-                            color,
-                            instance: inst,
-                            is_joker: false,
-                            index: number,
-                        })
-                    })
-                    .chain((0..=1).map(move |inst| {
-                        Var::Color(ColorBlock {
-                            number,
-                            instance: inst,
-                            is_joker: false,
-                            color,
-                        })
-                    }))
-                    .map(|v| Pos(v));
-                Either::Left(left)
-            }
-            Card::Joker => {
-                let right = iproduct!(0..=1, Color::iter(), 1..=13)
-                    .map(|(instance, color, number)| {
-                        Var::Color(ColorBlock {
-                            number,
-                            instance,
-                            is_joker: true,
-                            color,
-                        })
-                    })
-                    .chain(iproduct!(0..=1, Color::iter(), 1..=13).map(
-                        |(instance, color, index)| {
-                            Var::Num(NumberBlock {
-                                index,
-                                instance,
-                                is_joker: true,
-                                color,
-                            })
-                        },
-                    ))
-                    .map(|v| Pos(v));
-                Either::Right(right)
-            }
-        };
+fn encode_config(encoder: &mut DefaultEncoder<Var>, config: &Config, atleast: u32) {
+    let mut all_choosable = Vec::new();
 
-        encoder.add_constraint(ExactlyK { k: count, lits });
+    for (color, number) in iproduct!(Color::iter(), 1..=13) {
+        let lits = (0..=1)
+            .map(|instance| {
+                Pos(Var::Color(ColorBlock {
+                    number,
+                    instance,
+                    is_joker: false,
+                    color,
+                }))
+            })
+            .chain((0..=1).map(|instance| {
+                Pos(Var::Num(NumberBlock {
+                    color,
+                    instance,
+                    is_joker: false,
+                    index: number,
+                }))
+            }));
+
+        match config.cards.get(&Card::Normal { color, number }) {
+            Some(&count) if count > 0 => {
+                let choosable =
+                    (0..count).map(|i| Pos(Var::Chosen(Card::Normal { color, number }, i)));
+
+                all_choosable.extend(choosable.clone());
+
+                encoder.add_constraint({
+                    let mut same_cardinality_constraint = SameCardinality::new();
+                    same_cardinality_constraint
+                        .add_lits(choosable)
+                        .add_lits(lits);
+                    same_cardinality_constraint
+                });
+            }
+            _ => {
+                // If card doesn't appear in config it cannot be chosen and
+                // therefore not used.
+                encoder.add_constraint(ExactlyK { k: 0, lits });
+            }
+        }
     }
+
+    let joker_lits = iproduct!(Color::iter(), 1..=13, 0..=1)
+        .map(|(color, number, instance)| {
+            Pos(Var::Color(ColorBlock {
+                number,
+                instance,
+                is_joker: true,
+                color,
+            }))
+        })
+        .chain(
+            iproduct!(Color::iter(), 1..=13, 0..=1).map(|(color, index, instance)| {
+                Pos(Var::Num(NumberBlock {
+                    index,
+                    instance,
+                    is_joker: true,
+                    color,
+                }))
+            }),
+        );
+
+    match config.cards.get(&Card::Joker) {
+        Some(&count) if count > 0 => {
+            let choosable = (0..count).map(|i| Pos(Var::Chosen(Card::Joker, i)));
+
+            all_choosable.extend(choosable.clone());
+
+            encoder.add_constraint({
+                let mut same_cardinality_constraint = SameCardinality::new();
+                same_cardinality_constraint
+                    .add_lits(choosable)
+                    .add_lits(joker_lits);
+                same_cardinality_constraint
+            });
+        }
+        _ => {
+            // If card doesn't appear in config it cannot be chosen and
+            // therefore not used.
+            encoder.add_constraint(ExactlyK {
+                k: 0,
+                lits: joker_lits,
+            });
+        }
+    }
+
+    for c in &all_choosable {
+        match c.var() {
+            Var::Chosen(card, i) if *i > 0 => encoder.add_constraint(If {
+                cond: *c,
+                then: Pos(Var::Chosen(*card, i - 1)),
+            }),
+            _ => {}
+        }
+    }
+
+    encoder.add_constraint(AtleastK {
+        k: atleast,
+        lits: all_choosable.into_iter(),
+    });
 }
 
 #[derive(Debug)]
 pub struct Config {
     cards: HashMap<Card, u32>,
+}
+
+impl Config {
+    fn total(&self) -> u32 {
+        self.cards.values().sum()
+    }
 }
 
 impl Default for Config {
@@ -247,9 +314,32 @@ impl Default for Config {
     }
 }
 
-fn pretty_print_solution(model: &Model<Var>) {
-    const END_COLOR: &str = "\u{1b}[0m";
+fn pretty_print_config(config: &Config) {
+    for color in Color::iter() {
+        let mut nums = config
+            .cards
+            .iter()
+            .filter_map(|(card, count)| match card {
+                Card::Normal { color: c, number } if color == *c => {
+                    Some(vec![number; *count as usize])
+                }
+                _ => None,
+            })
+            .flatten()
+            .collect::<Vec<_>>();
+        nums.sort();
 
+        let s = nums.into_iter().map(|i| i.to_string()).join(" ");
+
+        println!("{}{}{}", color.ascii_color_code(), s, Color::END_COLOR);
+    }
+
+    if let Some(joker_count) = config.cards.get(&Card::Joker) {
+        println!("{}", "X".repeat(*joker_count as usize));
+    }
+}
+
+fn pretty_print_solution(model: &Model<Var>) {
     // number blocks
     for (color, instance) in iproduct!(Color::iter(), 0..=1) {
         let iter = (1..=13)
@@ -276,17 +366,18 @@ fn pretty_print_solution(model: &Model<Var>) {
                 Var::Color(_) => {
                     unreachable!()
                 }
+                Var::Chosen(..) => continue,
             };
 
             if b {
                 if card.is_joker {
-                    print!("{} X{}", card.color.ascii_color_code(), END_COLOR);
+                    print!("{} X{}", card.color.ascii_color_code(), Color::END_COLOR);
                 } else {
                     print!(
                         "{}{:>2}{}",
                         card.color.ascii_color_code(),
                         card.index,
-                        END_COLOR
+                        Color::END_COLOR
                     );
                 }
             } else {
@@ -323,17 +414,18 @@ fn pretty_print_solution(model: &Model<Var>) {
                     unreachable!()
                 }
                 Var::Color(color) => color,
+                Var::Chosen(..) => continue,
             };
 
             if b {
                 if card.is_joker {
-                    print!("{} X{}", card.color.ascii_color_code(), END_COLOR);
+                    print!("{} X{}", card.color.ascii_color_code(), Color::END_COLOR);
                 } else {
                     print!(
                         "{}{:>2}{}",
                         card.color.ascii_color_code(),
                         card.number,
-                        END_COLOR
+                        Color::END_COLOR
                     );
                 }
             } else {
@@ -342,6 +434,23 @@ fn pretty_print_solution(model: &Model<Var>) {
         }
         println!()
     }
+}
+
+fn count_cards_in_solution(model: &Model<Var>) -> usize {
+    model
+        .vars()
+        .filter(|v| matches!(v, Pos(Var::Chosen(..))))
+        .count()
+}
+
+fn try_solve(config: &Config, atleast: u32) -> Option<(Model<Var>, DefaultEncoder<Var>)> {
+    let mut encoder = DefaultEncoder::new();
+
+    encode_general_rules(&mut encoder);
+    encode_config(&mut encoder, config, atleast);
+
+    let model = encoder.solve();
+    model.map(|m| (m, encoder))
 }
 
 #[derive(structopt::StructOpt)]
@@ -353,27 +462,61 @@ struct Options {
 fn main() -> anyhow::Result<()> {
     let options = Options::from_args();
 
-    let mut encoder = DefaultEncoder::with_debug();
-
-    encode_general_rules(&mut encoder);
-
     let config = std::fs::read_to_string(options.path)
         .context("Reading input file failed.")?;
     let config = parser::parse_config(&config)
         .context("Parsing input failed.")?;
 
-    encode_config(&mut encoder, config);
-    
-    dbg!(&encoder.varmap);
+    pretty_print_config(&config);
 
-    encoder
-        .solver
-        .write_dimacs(&std::path::PathBuf::from("clauses.dimacs"))
-        .unwrap();
+    let total = config.total();
 
-    if let Some(model) = encoder.solve() {
-        //println!("{:?}", model.all_vars().collect::<Vec<_>>());
+    let mut min = 0;
+    let mut max = total + 1;
+
+    let mut best_model = None;
+    let mut best = 0;
+
+    loop {
+        let val = (min + max) / 2;
+
+        if val == min || val == max {
+            break;
+        }
+
+        println!("Try atleast {}", val);
+        if let Some(model) = try_solve(&config, val) {
+            let cards = count_cards_in_solution(&model.0) as u32;
+
+            println!("Possible solution! ({})", cards);
+
+            min = cards;
+            if val > best {
+                best = cards;
+                best_model = Some(model);
+            }
+        } else {
+            println!("Impossible!");
+            max = val;
+        }
+    }
+
+
+    if let Some((mut model, mut encoder)) = best_model {
         pretty_print_solution(&model);
+
+        encoder.solver.write_dimacs(&PathBuf::from("clauses.dimacs")).unwrap();
+
+        loop {
+            encoder.add_constraint(Or(model.vars().filter(|v| matches!(v.var(), Var::Chosen(..))).map(|v| !v)));
+            if let Some(new_model) = encoder.solve() {
+                println!("Alternative Model:");
+                pretty_print_solution(&new_model);
+                model = new_model;
+            } else {
+                break;
+            }
+        }
     } else {
         println!("No solution found!");
     }
